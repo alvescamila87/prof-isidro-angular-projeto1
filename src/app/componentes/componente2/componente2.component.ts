55a8113f4f759cabbdc0d3232c69622bc6cb1fa2
Camila <alvescamila87@gmail.com>
2024-07-24 12:15:25 -0300

    ngIf

--- src/app/componentes/componente2/componente2.component.ts
+++ src/app/componentes/componente2/componente2.component.ts
@@ -1,32 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from '../../servicos/post.service';
 import { Post } from '../../model/Post';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-componente2',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, NgIf],
   templateUrl: './componente2.component.html',
   styleUrl: './componente2.component.css'
 })
 export class Componente2Component implements OnInit {
 
+  public loading: boolean;
+
   public lista: Post[];
 
   public constructor(private service: PostService){
     this.lista = [];
+    this.loading = false;
   }
   ngOnInit(): void {
+    this.loading = true;
     this.service.consumirPosts().subscribe({
       next: (res: Post[]) => {
         console.log("Sucesso!");
         //console.log(res);
         this.lista = res;
+        this.loading = false;
       },
       error: (err: any) => {
         console.log("ERRO");
         console.log(err);
+        this.loading = false;
       }
     })
   }
