dfeab6758cf6b4e1e3f255bf291561f5eb8110a9
Camila <alvescamila87@gmail.com>
2024-07-23 21:23:02 -0300

    ngFor

--- src/app/componentes/componente2/componente2.component.ts
+++ src/app/componentes/componente2/componente2.component.ts
@@ -1,24 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from '../../servicos/post.service';
 import { Post } from '../../model/Post';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-componente2',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './componente2.component.html',
   styleUrl: './componente2.component.css'
 })
 export class Componente2Component implements OnInit {
 
-  public constructor(private service: PostService){
+  public lista: Post[];
 
+  public constructor(private service: PostService){
+    this.lista = [];
   }
   ngOnInit(): void {
     this.service.consumirPosts().subscribe({
       next: (res: Post[]) => {
         console.log("Sucesso!");
-        console.log(res);
+        //console.log(res);
+        this.lista = res;
       },
       error: (err: any) => {
         console.log("ERRO");
